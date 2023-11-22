15402f96ad5099fa3ef58a9413a9171cefe15927
yuanconghao <yuanconghao@51talk.com>
2023-11-22 14:46:22 +0800

    add llama2

--- server/utils/model.py
+++ server/utils/model.py
@@ -12,9 +12,9 @@ class ModelUtils(object):
             load_in_4bit=True,
             bnb_4bit_compute_dtype=torch.float16,
             bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True,
-            llm_int8_threshold=6.0,
-            llm_int8_has_fp16_weight=False,
+            # bnb_4bit_use_double_quant=True,
+            # llm_int8_threshold=6.0,
+            # llm_int8_has_fp16_weight=False,
         )
         if load_in_4bit:
             pass
@@ -24,12 +24,13 @@ class ModelUtils(object):
         # 加载base model
         model = AutoModelForCausalLM.from_pretrained(
             model_name_or_path,
-            load_in_4bit=load_in_4bit,
+            quantization_config=quantization_config,
             trust_remote_code=True,
-            low_cpu_mem_usage=True,
-            torch_dtype=torch.float16,
-            device_map='auto',
-            quantization_config=quantization_config
+            # load_in_4bit=load_in_4bit,
+            # low_cpu_mem_usage=True,
+            # torch_dtype=torch.float16,
+            # device_map='auto',
+
         )
 
         # 加载adapter
