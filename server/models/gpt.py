96f8ee863f0f3563cfd022531ad2795b05ccf0c8
yuanconghao <yuanconghao@51talk.com>
2023-11-27 13:11:57 +0800

    change temprature

--- server/models/gpt.py
+++ server/models/gpt.py
@@ -15,7 +15,7 @@ class GPTGenerator:
         response = openai.chat.completions.create(
             model=model,
             messages=messages,
-            temperature=0,
+            temperature=0.5,
             max_tokens=2048,
             stream=stream,
         )
