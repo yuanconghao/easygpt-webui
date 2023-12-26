5aae1113805d2aa1f9b002f9473e4632507dd470
yuanconghao <yuanconghao@51talk.com>
2023-12-26 18:02:47 +0800

    add ai speaking teacher

--- client/js/speaking_teacher.js
+++ client/js/speaking_teacher.js
@@ -1,6 +1,7 @@
 var ai_teacher_json = {
     "introduce": {
         "src": "https://online-kcs.51talk.com/chatbot/nGbxB2v4bEBPIkQR",
+        "scene": "互相介绍",
         "goal": {
             "cn": "向你的朋友介绍你自己",
             "en": "Introduce yourself to your friend",
@@ -26,6 +27,7 @@ var ai_teacher_json = {
     },
     "freely": {
         "src": "https://online-kcs.51talk.com/chatbot/3JM8zxbh7XTjsH6o",
+        "scene": "自由对话",
         "goal": {
             "cn": "自由地聊天",
             "en": "Chat freely",
@@ -47,6 +49,7 @@ var ai_teacher_json = {
     },
     "interest": {
         "src": "https://online-kcs.51talk.com/chatbot/DzLWTb5WLUzBPtte",
+        "scene": "讨论兴趣爱好",
         "goal": {
             "cn": "讨论兴趣爱好",
             "en": "Discuss hobbies and interests",
@@ -72,6 +75,7 @@ var ai_teacher_json = {
     },
     "food": {
         "src": "https://online-kcs.51talk.com/chatbot/LFsV8nReFyAtnS5L",
+        "scene": "讨论食物",
         "goal": {
             "cn": "点餐，聊一聊食物",
             "en": "Order food and have a conversation about food",
@@ -93,6 +97,106 @@ var ai_teacher_json = {
                 "cn": "与服务员分享一段关于食物的记忆或经历",
                 "en": "Share a food memory or experience with the waiter"
             },
+        ]
+    },
+    "order_food": {
+        "src": "https://online-kcs.51talk.com/chatbot/yJJTDUshKarcDGnm",
+        "scene": "在餐厅点餐",
+        "goal": {
+            "cn": "点菜",
+            "en": "Order food",
+        },
+        "challenge": [
+            {
+                "cn": "要一份中号牛排和中号薯条",
+                "en": "Request a medium steak and medium fries"
+            },
+            {
+                "cn": "在旁边指定你喜欢的番茄酱",
+                "en": "Specify a preference for ketchup sauce on the side"
+            },
+            {
+                "cn": "要一杯可乐",
+                "en": "Request a glass of Coke"
+            },
+            {
+                "cn": "询问一下洗手间的位置",
+                "en": "Inquire about the location of the restroom"
+            },
+        ]
+    },
+    "shopping": {
+        "src": "https://online-kcs.51talk.com/chatbot/xncu5wP6jtWRkroL",
+        "scene": "购物买衣服",
+        "goal": {
+            "cn": "询问一个特定的商品，并购买",
+            "en": "Inquire about a special cloth and make a purchase",
+        },
+        "challenge": [
+            {
+                "cn": "询问销售人员某一个特定物品的可用性",
+                "en": "Ask the salesperson about the availability of a specific cloth"
+            },
+            {
+                "cn": "询问该物品的价格和质量",
+                "en": "Inquire about the price and quality of the cloth"
+            },
+            {
+                "cn": "向销售人员要求折扣和促销",
+                "en": "Ask the salesperson for a discount or promotion"
+            },
+            {
+                "cn": "购买并感谢销售人员的帮助",
+                "en": "Make a purchase and thank the salesperson for their help"
+            },
+        ]
+    },
+    "ask_directions": {
+        "src": "https://online-kcs.51talk.com/chatbot/F0D4zwkTLeXrv03y",
+        "scene": "问路",
+        "goal": {
+            "cn": "询问附近旅游景点的方向",
+            "en": "Ask for directions to a nearby tourist attraction",
+        },
+        "challenge": [
+            {
+                "cn": "自我介绍一下，说明你是游客",
+                "en": "Introduce yourself and explain that you are a tourist"
+            },
+            {
+                "cn": "向当地居民询问最近的旅游景点的方向",
+                "en": "Ask the local resident for directions to the nearest tourist attraction"
+            },
+            {
+                "cn": "向当地居民询问该地区其他值得参观的地方",
+                "en": "Ask the local resident for recommendations on other places to visit in the area"
+            },
+            {
+                "cn": "感谢当地居民的帮助，说再见",
+                "en": "Thank the local resident for their help and say goodbye"
+            },
+        ]
+    },
+    "weather": {
+        "src": "https://online-kcs.51talk.com/chatbot/dY24ee86uJ6WUt03",
+        "scene": "讨论天气",
+        "goal": {
+            "cn": "讨论天气和公园活动",
+            "en": "Discuss the weather and park activities",
+        },
+        "challenge": [
+            {
+                "cn": "评论一下美丽的天气，问问公园管理员今天是不是公园里的好天气",
+                "en": "Comment on the beautiful weather and ask the park ranger if it's a good day to be in the park"
+            },
+            {
+                "cn": "根据天气情况，向公园管理员询问公园活动的建议",
+                "en": "Ask the park ranger for recommendations on park activities based on the weather"
+            },
+            {
+                "cn": "讨论花时间在户外的好处，并向公园管理员询问在公园里保持安全的建议",
+                "en": "Discuss the benefits of spending time outdoors and ask the park ranger for tips on staying safe in the park"
+            }
         ]
     }
 }
