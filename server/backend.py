e596e8474451dbe28eb61eac8b7413f67c32e349
yuanconghao <yuanconghao@51talk.com>
2024-04-15 10:33:11 +0800

    add geshui

--- server/backend.py
+++ server/backend.py
@@ -61,6 +61,10 @@ class Backend_Api:
                 'function': self._user_info_bind,
                 'methods': ['POST']
             },
+            '/backend-api/v2/geshui_calc': {
+                'function': self._geshui_calc,
+                'methods': ['POST']
+            },
         }
 
         llama2 = config['llama2']
@@ -343,3 +347,55 @@ class Backend_Api:
             "url": "https://kcs.51talk.com/easygpt/uploads/51_test.jpg",
             "message": "感谢您的确认。已经为您安排了课程顾问，会第一时间联系您。您也可以扫描下面的二维码添加课程顾问。课程顾问会为您提供详细的课程介绍和咨询服务。"
         }
+
+
+    def _geshui_calc(self):
+        income = float(request.json['phone'])
+        zhuanxiang = int(request.json['zhuanxiang'])
+
+        yijin = income * 0.12
+        wuxian = income * 0.1
+
+        rate = [0.03, 0.1, 0.2, 0.25, 0.3, 0.35, 0.45]
+        base = [0, 36000, 144000, 300000, 420000, 660000, 960000]
+        temp_total = []
+        susuan = []
+
+        for i in range(0, 6, 1):
+            temp_total.append((base[i + 1] - base[i]) * rate[i])
+
+        for i in range(1, 7, 1):
+            sub_total = 0
+            for j in range(i, 0, -1):
+                sub_total = sub_total + temp_total[j - 1]
+            susuan.append(base[i] * rate[i] - sub_total)
+
+        income = income - wuxian - yijin - zhuanxiang - 5000
+
+        # print("%.2f" % income)
+        pre_tax = 0
+        for k in range(1, 13, 1):
+            income = (18000 - wuxian - yijin - zhuanxiang - 5000) * k
+            #	print("第 %d 月应纳税额：%.2f" % (k, income))
+
+            for i in range(len(base)):
+                if income > base[i]:
+                    continue
+                else:
+                    if i < 2:
+                        tax = income * 0.03 - pre_tax
+                        pre_tax = income * 0.03
+                        # print("tax: %.2f" % tax)
+                    else:
+                        tax = income * rate[i - 1] - susuan[i - 2] - pre_tax
+                        pre_tax = pre_tax + tax
+                        # print("tax: %.2f" % tax)
+                    #			print("%d, %d, %d" % (i, base[i], susuan[i-2]))
+                    break
+
+        # print("total tax : %.2f" % pre_tax)
+        return {
+            'total_tax': pre_tax
+        }
+
+
